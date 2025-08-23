88513824c4497c8c778f6b3d1001abd5238d3e1b
parides55 <andreasparides5@gmail.com>
2025-08-23 17:17:28 +0300

    add the configuration for the flask_babel and make few translations for test.

--- run.py
+++ run.py
@@ -1,9 +1,20 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 
 app = Flask(__name__)
 
+app.config['BABEL_DEFAULT_LOCALE'] = 'en'   # default language
+app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'el']  # add your languages
+
+
+# Define locale selector function
+def get_locale():
+    return request.args.get('lang') or request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
+
+# Pass it into Babel
+babel = Babel(app, locale_selector=get_locale)
 
 @app.route('/')
 def index():
